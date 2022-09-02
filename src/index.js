0ee9ff76aa3a33ee68eacb2d055f83e9f1d06996
Лана Константинова <lnk.konst@gmail.com>
2022-09-03 00:46:47 +0300

    output format update

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ const readFile = (filepath) => {
     return JSON.parse(content);
 };
 
+const valToString = (value) => {
+    if (Array.isArray(value)) return `${JSON.stringify(value)}`;
+    return `${JSON.stringify(value, null, ' ')}`;
+}
 
 const compareFiles = (data1, data2) => {
     let res = [];
@@ -21,20 +25,22 @@ const compareFiles = (data1, data2) => {
 
     for (const key of keys) {
         const value1 = data1[key];
+        const valAsString1 = valToString(value1);
         const value2 = data2[key];
+        const valAsString2 = valToString(value2);
         if (Object.hasOwn(data1, key)) {
             if (Object.hasOwn(data2, key)) {
                 if (_.isEqual(value1, value2)) {
-                    res.push(`  ${key}: ${JSON.stringify(value1)}`);
+                    res.push(`  ${key}: ${valAsString1}`);
                 } else {
-                    res.push(`- ${key}: ${JSON.stringify(value1)}`);
-                    res.push(`+ ${key}: ${JSON.stringify(value2)}`);
+                    res.push(`- ${key}: ${valAsString1}`);
+                    res.push(`+ ${key}: ${valAsString2}`);
                 }
             } else {
-                res.push(`- ${key}: ${JSON.stringify(value1)}`);
+                res.push(`- ${key}: ${valAsString1}`);
             }
         } else {
-            res.push(`+ ${key}: ${JSON.stringify(value2)}`);
+            res.push(`+ ${key}: ${valAsString2}`);
         }
     }
     return(res.join('\n'));
@@ -43,6 +49,7 @@ const compareFiles = (data1, data2) => {
 const genDiff = (filepath1, filepath2) => {
     const content1 = readFile(getAbsolutePath(filepath1));
     const content2 = readFile(getAbsolutePath(filepath2));
+    console.log(getAbsolutePath(filepath1))
     return compareFiles(content1, content2);
 };
 
