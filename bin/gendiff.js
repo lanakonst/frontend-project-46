c0a7782544f82493ced563600c7a95b81425cd22
Лана Константинова <lnk.konst@gmail.com>
2022-09-03 01:53:55 +0300

    linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,14 @@
 #!/usr/bin/env node
-import { Command } from "commander";
+import { Command } from 'commander';
 import genDiff from '../src/index.js';
-import fs from 'fs';
-import path from 'path';
-import process from 'process';
-import { receiveMessageOnPort } from "worker_threads";
+
 const program = new Command();
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('1.0.0')
-    .arguments('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
-    .parse();
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)))
+  .parse();
