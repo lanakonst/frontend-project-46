6e426ddf68a0029db313be9c45761168ac9dfd85
Лана Константинова <lnk.konst@gmail.com>
2022-09-13 21:42:03 +0300

    lint

--- src/makeTree.js
+++ src/makeTree.js
@@ -6,16 +6,16 @@ const createComparisionTree = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
     if (!Object.hasOwn(data1, key)) {
-        return { key, genDiffStatus: 'added', children: value2 };
-    } else if (!Object.hasOwn(data2, key)) {
-        return { key, genDiffStatus: 'removed', children: value1 };
-    } else if (_.isEqual(value1, value2)) {
-        return { key, genDiffStatus: 'unchanged', children: value1 };
-    } else if (_.isObject(value1) && _.isObject(value2)) {
-        return { key, genDiffStatus: '', children: createComparisionTree(value1, value2) };
+      return { key, genDiffStatus: 'added', children: value2 };
+    } if (!Object.hasOwn(data2, key)) {
+      return { key, genDiffStatus: 'removed', children: value1 };
+    } if (_.isEqual(value1, value2)) {
+      return { key, genDiffStatus: 'unchanged', children: value1 };
+    } if (_.isObject(value1) && _.isObject(value2)) {
+      return { key, genDiffStatus: '', children: createComparisionTree(value1, value2) };
     }
     return { key, genDiffStatus: 'updated', children: [value1, value2] };
-    });
+  });
 
   return res;
 };
