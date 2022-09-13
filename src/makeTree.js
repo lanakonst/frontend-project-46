d5dc75ef9a68d4b4eb765b72603cbab3d7bd8298
Лана Константинова <lnk.konst@gmail.com>
2022-09-13 21:31:03 +0300

    simplifying makeTree

--- src/makeTree.js
+++ src/makeTree.js
@@ -5,6 +5,21 @@ const createComparisionTree = (data1, data2) => {
   const res = keys.reduce((acc, key) => {
     const value1 = data1[key];
     const value2 = data2[key];
+
+    if (!Object.hasOwn(data1, key)) {
+      acc.push({ key, genDiffStatus: 'added', children: value2 });
+    } else if (!Object.hasOwn(data2, key)) {
+      acc.push({ key, genDiffStatus: 'removed', children: value1 });
+    } else if (_.isEqual(value1, value2)) {
+      acc.push({ key, genDiffStatus: 'unchanged', children: value1 });
+    } else if (_.isObject(value1) && _.isObject(value2)) {
+      acc.push({ key, genDiffStatus: '', children: createComparisionTree(value1, value2) });
+    } else {
+      acc.push({ key, genDiffStatus: 'updated', children: [value1, value2] });
+    }
+    return acc;
+  }, []);
+    /*
     if (Object.hasOwn(data1, key)) {
       if (Object.hasOwn(data2, key)) {
         if (_.isEqual(value1, value2)) {
@@ -21,7 +36,7 @@ const createComparisionTree = (data1, data2) => {
       acc.push({ key, genDiffStatus: 'added', children: value2 });
     }
     return acc;
-  }, []);
+  }, []); */
 
   return res;
 };
