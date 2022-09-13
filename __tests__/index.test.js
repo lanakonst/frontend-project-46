d880a93ae783eb5c1f609e220e4bb3023bb31432
Лана Константинова <lnk.konst@gmail.com>
2022-09-13 20:13:55 +0300

    json test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,6 +12,7 @@ const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8'
 
 const expectedResStylish = readFile('compare1n2.Stylish.txt');
 const expectedResPlain = readFile('compare1n2.Plain.txt');
+const expectedResJSON = readFile('compare1n2.JSON.txt');
 
 test('two json files. Stylish', () => {
   const testFilePath1 = getFixturePath('file1.test.json');
@@ -36,3 +37,16 @@ test('yaml files. Plain', () => {
   const testFilePath2 = getFixturePath('file2.test.yml');
   expect(genDiff(testFilePath1, testFilePath2, 'plain')).toEqual(expectedResPlain);
 });
+
+test('two json files. JSON', () => {
+  const testFilePath1 = getFixturePath('file1.test.json');
+  const testFilePath2 = getFixturePath('file2.test.json');
+  expect(genDiff(testFilePath1, testFilePath2, 'json')).toEqual(expectedResJSON);
+});
+
+test('yaml files. JSON', () => {
+  const testFilePath1 = getFixturePath('file1.test.yaml');
+  const testFilePath2 = getFixturePath('file2.test.yml');
+  expect(genDiff(testFilePath1, testFilePath2, 'json')).toEqual(expectedResJSON);
+});
+
