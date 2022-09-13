a5edb06160d33a2ffdedc2d3e474afa904901585
Лана Константинова <lnk.konst@gmail.com>
2022-09-13 21:40:04 +0300

    no-push makeTree

--- src/makeTree.js
+++ src/makeTree.js
@@ -2,41 +2,20 @@ import _ from 'lodash';
 
 const createComparisionTree = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-  const res = keys.reduce((acc, key) => {
+  const res = keys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
-
     if (!Object.hasOwn(data1, key)) {
-      acc.push({ key, genDiffStatus: 'added', children: value2 });
+        return { key, genDiffStatus: 'added', children: value2 };
     } else if (!Object.hasOwn(data2, key)) {
-      acc.push({ key, genDiffStatus: 'removed', children: value1 });
+        return { key, genDiffStatus: 'removed', children: value1 };
     } else if (_.isEqual(value1, value2)) {
-      acc.push({ key, genDiffStatus: 'unchanged', children: value1 });
+        return { key, genDiffStatus: 'unchanged', children: value1 };
     } else if (_.isObject(value1) && _.isObject(value2)) {
-      acc.push({ key, genDiffStatus: '', children: createComparisionTree(value1, value2) });
-    } else {
-      acc.push({ key, genDiffStatus: 'updated', children: [value1, value2] });
+        return { key, genDiffStatus: '', children: createComparisionTree(value1, value2) };
     }
-    return acc;
-  }, []);
-    /*
-    if (Object.hasOwn(data1, key)) {
-      if (Object.hasOwn(data2, key)) {
-        if (_.isEqual(value1, value2)) {
-          acc.push({ key, genDiffStatus: 'unchanged', children: value1 });
-        } else if (_.isObject(value1) && _.isObject(value2)) {
-          acc.push({ key, genDiffStatus: '', children: createComparisionTree(value1, value2) });
-        } else {
-          acc.push({ key, genDiffStatus: 'updated', children: [value1, value2] });
-        }
-      } else {
-        acc.push({ key, genDiffStatus: 'removed', children: value1 });
-      }
-    } else {
-      acc.push({ key, genDiffStatus: 'added', children: value2 });
-    }
-    return acc;
-  }, []); */
+    return { key, genDiffStatus: 'updated', children: [value1, value2] };
+    });
 
   return res;
 };
