6f225cd64bcc02d07636f041840995ae350f2961
Лана Константинова <lnk.konst@gmail.com>
2022-09-18 21:56:14 +0300

    less sign

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 const interval = 4;
 
-const makeSpaces = (sign, depth) => (sign === '+ ' || sign === '- ' ? (' ').repeat(interval * depth - 2) : (' ').repeat(interval * depth));
+const makeSpaces = (depth) => (' ').repeat(interval * depth - 2);
 
 const typeObject = (obj, depth) => {
   const line = _.reduce(obj, (acc, value, key) => {
@@ -17,7 +17,7 @@ const typeObject = (obj, depth) => {
 
 const makeLine = (sign, key, value, depth) => {
   const spacesBeforeBreckets = (' ').repeat(interval * depth);
-  const spaces = makeSpaces(sign, depth);
+  const spaces = makeSpaces(depth);
   if (_.isObject(value)) {
     return `${spaces}${sign}${key}: {\n${typeObject(value, depth + 1)}\n${spacesBeforeBreckets}}`;
   }
@@ -29,10 +29,10 @@ const stylish = (data, depth = 1) => {
     const { key, status } = obj;
     const spacesBeforeBreckets = (' ').repeat(interval * depth);
     if (status === 'nested') {
-      const sign = '';
-      const spaces = makeSpaces(sign, depth);
+      //const sign = '';
+      const spaces = makeSpaces(depth);
       const { children } = obj;
-      return _.concat(acc, `${spaces}${sign}${key}: {\n${stylish(children, depth + 1)}\n${spacesBeforeBreckets}}`);
+      return _.concat(acc, `${spaces}  ${key}: {\n${stylish(children, depth + 1)}\n${spacesBeforeBreckets}}`);
     }
     const { value } = obj;
     if (status === 'updated') {
@@ -40,9 +40,9 @@ const stylish = (data, depth = 1) => {
       const newValue = value[1];
       return _.concat(acc, `${makeLine('- ', key, initValue, depth)}\n${makeLine('+ ', key, newValue, depth)}`);
     }
-    if (status === 'removed') return _.concat(acc, makeLine('- ', key, value, depth));
-    if (status === 'added') return _.concat(acc, makeLine('+ ', key, value, depth));
-    return _.concat(acc, makeLine('', key, value, depth));
+    if (status === 'unchanged') return _.concat(acc, makeLine('  ', key, value, depth));
+    const sign = status === 'removed' ? '- ' : '+ ';
+    return _.concat(acc, makeLine(sign, key, value, depth));
   }, []);
   return tree.join('\n');
 };
