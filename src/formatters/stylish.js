8dec7c1629ab651efc27733ad242597032ee4659
Лана Константинова <lnk.konst@gmail.com>
2022-09-22 04:23:03 +0300

    no makeLine

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,44 +4,38 @@ const interval = 4;
 
 const makeSpaces = (depth) => (' ').repeat(interval * depth - 2);
 
-const typeObject = (obj, depth) => {
+const stringify = (obj, depth) => {
   const line = _.reduce(obj, (acc, value, key) => {
     const spaces = (' ').repeat(interval * depth);
     if (_.isObject(value)) {
-      return _.concat(acc, `${spaces}${key}: {\n${typeObject(value, depth + 1)}\n${spaces}}`);
+      return _.concat(acc, `${spaces}${key}: {\n${stringify(value, depth + 1)}\n${spaces}}`);
     }
     return _.concat(acc, `${spaces}${key}: ${value}`);
   }, []);
   return line.join('\n');
 };
 
-const makeLine = (sign, key, value, depth) => {
-  const spacesBeforeBreckets = (' ').repeat(interval * depth);
-  const spaces = makeSpaces(depth);
-  if (_.isObject(value)) {
-    return `${spaces}${sign}${key}: {\n${typeObject(value, depth + 1)}\n${spacesBeforeBreckets}}`;
-  }
-  return `${spaces}${sign}${key}: ${value}`;
-};
-
 const stylish = (data, depth = 1) => {
   const tree = data.flatMap((obj) => {
     const { key, status } = obj;
     const spacesBeforeBreckets = (' ').repeat(interval * depth);
+    const spaces = makeSpaces(depth);
     if (status === 'nested') {
-      const spaces = makeSpaces(depth);
       const { children } = obj;
       return `${spaces}  ${key}: {\n${stylish(children, depth + 1)}\n${spacesBeforeBreckets}}`;
     }
-    const { value } = obj;
+
     if (status === 'updated') {
-      const initValue = value[0];
-      const newValue = value[1];
-      return `${makeLine('- ', key, initValue, depth)}\n${makeLine('+ ', key, newValue, depth)}`;
+      const initValue = _.isObject(obj.value[0]) ? `{\n${stringify(obj.value[0], depth + 1)}\n${spacesBeforeBreckets}}` : obj.value[0];
+      const newValue = _.isObject(obj.value[1]) ? `{\n${stringify(obj.value[1], depth + 1)}\n${spacesBeforeBreckets}}` : obj.value[1];
+      return `${spaces}- ${key}: ${initValue}\n${spaces}+ ${key}: ${newValue}`;
     }
-    if (status === 'unchanged') return makeLine('  ', key, value, depth);
-    const sign = status === 'removed' ? '- ' : '+ ';
-    return makeLine(sign, key, value, depth);
+    const value = _.isObject(obj.value) ? `{\n${stringify(obj.value, depth + 1)}\n${spacesBeforeBreckets}}` : obj.value;
+    if (status === 'removed') return `${spaces}- ${key}: ${value}`;
+    if (status === 'added') return `${spaces}+ ${key}: ${value}`;
+    // unchanged:
+
+    return `${spaces}  ${key}: ${value}`;
   });
   return tree.join('\n');
 };
