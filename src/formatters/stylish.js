94b964a102a2fe26cee5c9dab8958dab6b0f1b78
Лана Константинова <lnk.konst@gmail.com>
2022-09-26 06:39:26 +0300

    object check in stringify

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,37 +2,36 @@ import _ from 'lodash';
 
 const interval = 4;
 
-const makeSpaces = (depth) => (' ').repeat(interval * depth - 2);
+const makeSpaces = (depth) => (' ').repeat(interval * depth);
 
 const stringify = (value, depth) => {
+  if (!_.isObject(value)) return String(value);
   const line = _.reduce(value, (acc, val, key) => {
-    const spaces = (' ').repeat(interval * depth);
+    const innerSpaces = makeSpaces(depth + 1);
     if (_.isObject(val)) {
-      return _.concat(acc, `${spaces}${key}: {\n${stringify(val, depth + 1)}\n${spaces}}`);
+      return _.concat(acc, `${innerSpaces}${key}: ${stringify(val, depth + 1)}`);
     }
-    return _.concat(acc, `${spaces}${key}: ${val}`);
+    return _.concat(acc, `${innerSpaces}${key}: ${val}`);
   }, []);
-  return line.join('\n');
+  const spaces = makeSpaces(depth);
+  return `{\n${line.join('\n')}\n${spaces}}`;
 };
 
 const stylish = (data, depth = 1) => {
   const tree = data.flatMap((obj) => {
     const { key, status } = obj;
-    const spacesBeforeBreckets = (' ').repeat(interval * depth);
-    const spaces = makeSpaces(depth);
+    const spacesBeforeBreckets = makeSpaces(depth);
+    const spaces = (' ').repeat(interval * depth - 2);
     if (status === 'nested') {
       const { children } = obj;
       return `${spaces}  ${key}: {\n${stylish(children, depth + 1)}\n${spacesBeforeBreckets}}`;
     }
 
     if (status === 'updated') {
-      const [initValue, newValue] = obj.value.map((val) => {
-        if (_.isObject(val)) return `{\n${stringify(val, depth + 1)}\n${spacesBeforeBreckets}}`;
-        return val;
-      });
+      const [initValue, newValue] = obj.value.map((val) => stringify(val, depth));
       return `${spaces}- ${key}: ${initValue}\n${spaces}+ ${key}: ${newValue}`;
     }
-    const value = _.isObject(obj.value) ? `{\n${stringify(obj.value, depth + 1)}\n${spacesBeforeBreckets}}` : obj.value;
+    const value = stringify(obj.value, depth);
     if (status === 'removed') return `${spaces}- ${key}: ${value}`;
     if (status === 'added') return `${spaces}+ ${key}: ${value}`;
     // unchanged:
